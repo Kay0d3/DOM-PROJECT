e1838d2023ecd219dc8672ad76e1981e078ef56a
oluwadara <oluwadaraafolabi4@gmail.com>
2023-01-18 23:11:02 +0100

    Made some new changes to the styling and functionality

--- item.js
+++ item.js
@@ -2,14 +2,13 @@ let myForm = document.getElementById("my-form");
 let myList = document.getElementById("my-list");
 let mySearch = document.getElementById("search");
 
-// 1. I CALLED THE SUBMIT BTN, which you didnt call
 const submitBtn = document.getElementById("submitBtn");
 
 myForm.addEventListener("submit", addItem);
 myList.addEventListener("click", remeoveItem);
 mySearch.addEventListener("keydown", searchItem);
 
-// 2. This is suppose to be outside of the addItem function incase you need to use that inputValue again. and i didnt added the .value at the end of this.
+
 let inputValue = document.getElementById("text");
 
 submitBtn.addEventListener("click", addItem());
@@ -21,13 +20,9 @@ function addItem(e) {
   let deleteBtn = document.createElement("button");
   deleteBtn.className = "delete";
   deleteBtn.textContent = "X";
-
-  // 3. i added the .value here
   newLi.textContent = inputValue.value;
   newLi.appendChild(deleteBtn);
   myList.appendChild(newLi);
-
-  //4. Then lastly the inputvalue should be the last thing after all other functions have performed their actions, so you can now clear
   inputValue.value = "";
 }
 
@@ -47,6 +42,8 @@ function searchItem(e) {
     var itemName = item.firstChild.textContent;
     if (itemName.toLowerCase().indexOf(text) != -1) {
       item.style.display = "block";
+      item.style.display = 'flex'
+      item.style.justifyContent = 'space-between'
     } else {
       item.style.display = "none";
     }
